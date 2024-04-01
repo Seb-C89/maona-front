cfdc37dc5a4837ff307497ef24d7f1085e17ad9a
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2024-04-01 12:23:38 +0200

    fonctionnel

--- src/pages/Authentification/Authentification.jsx
+++ src/pages/Authentification/Authentification.jsx
@@ -1,11 +1,19 @@
-const Authentification = () => {
+import { useState, useEffect } from 'react';
 
-  return (
-    <main >
-     <h1>Auth</h1>
-     <p> OAuth 2.0</p>
-    </main>
-  );
+const Authentification = () => {
+  const [mail, setMail] = useState(document.cookie.split("; ").find((row) => row.startsWith("mail="))?.split("=")[1]);
+  
+return (
+  <main >
+    <h1>Auth</h1>
+    <p> OAuth 2.0</p>
+    {
+      mail
+      ? <p>YOU ARE {mail}</p>
+      : <p><a href="http://localhost:8000/authentification/auth">GITHUB OAuth: CLICK HERE</a></p>
+    }
+  </main>
+);
 };
 
 export default Authentification;
